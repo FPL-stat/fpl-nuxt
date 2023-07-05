951f5e093d908859af6785419a5167e35a58c66e
Dave <bigwaive@gmail.com>
2023-07-05 22:24:30 +0200

    remove pagination from player page

--- pages/players.vue
+++ pages/players.vue
@@ -1,61 +1,90 @@
-<script setup lang='ts'>
-import type { IPlayer } from '../types'
+<script setup lang="ts">
+import type { IPlayer } from "../types";
 
-const { data, pending, error, refresh } = await useLazyFetch<IPlayer[]>('/api/players', { server: false })
+const { data, pending, error, refresh } = await useLazyFetch<IPlayer[]>(
+  "/api/players",
+  { server: false }
+);
 
-const players = ref<IPlayer[]>([])
-const tableFilter = ref('')
-const page = ref(1)
-const pageCount = 9
+const players = ref<IPlayer[]>([]);
+const tableFilter = ref("");
+const page = ref(1);
+const pageCount = 9;
 
 const columns = [
-  { key: 'second_name', label: 'Second Name', sortable: true },
-  { key: 'first_name', label: 'First Name', sortable: true },
-  { key: 'total_points', label: 'Total Points', sortable: true },
-  { key: 'starts', label: 'Starts', sortable: true },
-  { key: 'value_season', label: 'Value (Season)', sortable: true },
-  { key: 'selected_by_percent', label: 'Selected By (%)', sortable: true },
-]
+  { key: "second_name", label: "Second Name", sortable: true },
+  { key: "first_name", label: "First Name", sortable: true },
+  { key: "total_points", label: "Total Points", sortable: true },
+  { key: "starts", label: "Starts", sortable: true },
+  { key: "value_season", label: "Value (Season)", sortable: true },
+  { key: "selected_by_percent", label: "Selected By (%)", sortable: true },
+];
+
 
 const filteredRows = computed(() => {
   if (!tableFilter.value) {
-    return players.value
+    return players.value.sort((a, b) => {
+      const nameA = a.second_name.toUpperCase();
+      const nameB = b.second_name.toUpperCase();
+      if (nameA < nameB) {
+        return -1; // a should come before b
+      }
+      if (nameA > nameB) {
+        return 1; // a should come after b
+      }
+      return 0;
+    });
   }
   return players.value.filter((player) => {
     return Object.values(player).some((value) => {
-      return String(value).toLowerCase().includes(tableFilter.value.toLowerCase())
-    })
-  })
-})
+      return String(value)
+        .toLowerCase()
+        .includes(tableFilter.value.toLowerCase());
+    });
+  });
+});
 
-const rows = computed(() => {
-  // return teams.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-  return filteredRows.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
+//const paginatedRows = computed(() => {
+//  return filteredRows.value.slice(
+//    (page.value - 1) * pageCount,
+//    page.value * pageCount
+//  );
+//});
 
-watch(data, newData => {
-  if (newData) players.value = newData
-})
+watch(data, (newData) => {
+  if (newData) players.value = newData;
+});
 
 onMounted(() => {
   if (data.value) {
-    players.value = data.value
+    players.value = data.value;
   }
-})
-
+});
 </script>
 
 <template>
-  <section class='p-8'>
-  
-    <div class='w-60'>
-      <UInput v-model='tableFilter' placeholder='search...' icon="i-heroicons-magnifying-glass-20-solid" />
+  <section class="p-8">
+    <div class="w-60">
+      <UInput
+        v-model="tableFilter"
+        placeholder="search..."
+        icon="i-heroicons-magnifying-glass-20-solid"
+      />
     </div>
 
-    <UTable class='overflow-auto' :loading='pending' :columns='columns' :rows='rows' />
+    <UTable
+      class="overflow-auto"
+      :loading="pending"
+      :columns="columns"
+      :rows="filteredRows"
+    >
+    </UTable>
 
-    <UPagination v-show='tableFilter.length === 0' v-model="page" :page-count="pageCount" :total="players.length" />
-
-
+    <!-- <UPagination -->
+    <!--   v-show="tableFilter.length === 0" -->
+    <!--   v-model="page" -->
+    <!--   :page-count="pageCount" -->
+    <!--   :total="players.length" -->
+    <!-- /> -->
   </section>
 </template>
