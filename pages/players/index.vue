768909032175322bdd4ad88742328933eb750e09
Dave <bigwaive@gmail.com>
2023-07-09 18:52:10 +0200

    display grid loading overlay

--- pages/players/index.vue
+++ pages/players/index.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { AgGridVue } from "ag-grid-vue3";
-import type { GridApi, GridReadyEvent, ICellRendererParams } from "ag-grid-community";
+import type {
+  GridApi,
+  GridReadyEvent,
+  ICellRendererParams,
+} from "ag-grid-community";
 import { reactive } from "vue";
 
 import { IPlayer } from "~/types";
 
-const router = useRouter()
+const router = useRouter();
 
 const colorMode = useColorMode();
 const isDark = computed({
@@ -17,37 +21,41 @@ const isDark = computed({
   },
 });
 
-const { data, pending } = useAsyncData<IPlayer[]>("players", () =>
+const { data, pending } = useLazyAsyncData<IPlayer[]>("players", () =>
   $fetch("/api/players")
 );
 
-function linkRenderer (params: ICellRendererParams<IPlayer>) {
-	const route = {
-		name: 'players-code',
-		params: { code: params.data.code }
-	}
-	const link = document.createElement("a")
-	link.href = router.resolve(route).href
-	link.innerText = params.value
-	link.addEventListener('click', e => {
-		e.preventDefault()
-		router.push(route)
-	})
-	return link
+function linkRenderer(params: ICellRendererParams<IPlayer>) {
+  const route = {
+    name: "players-code",
+    params: { code: params.data?.code },
+  };
+  const link = document.createElement("a");
+  link.href = router.resolve(route).href;
+  link.innerText = params.value;
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    router.push(route);
+  });
+  return link;
 }
 
+const tableFilter = ref("");
 
-const tableFilter = ref('')
-
-const gridApi = ref<GridApi|null>(null);
+const gridApi = ref<GridApi | null>(null);
 const onGridReady = (params: GridReadyEvent) => {
   gridApi.value = params.api;
 };
 
-const rowData = reactive({ rows: <IPlayer[]>[] });
+const rowData = reactive({ rows: <IPlayer[]|null>null });
 const columnDefs = reactive({
   value: [
-    { headerName: "Second Name", field: "second_name", resizable: true, cellRenderer: linkRenderer },
+    {
+      headerName: "Second Name",
+      field: "second_name",
+      resizable: true,
+      cellRenderer: linkRenderer,
+    },
     { headerName: "First Name", field: "first_name" },
     { headerName: "Total Points", field: "total_points" },
     { headerName: "Starts", field: "starts", minWidth: 100 },
@@ -63,21 +71,11 @@ const defaultColDef = {
 };
 
 function onTableFilterChange() {
-  if (gridApi.value) gridApi.value.setQuickFilter(tableFilter.value)
+  if (gridApi.value) gridApi.value.setQuickFilter(tableFilter.value);
 }
-
-
-
 
 watch(data, (newData) => {
   if (newData) rowData.rows = newData;
-});
-
-onMounted(async () => {
-  if (data.value) {
-    rowData.rows = data.value;
-  }
-  console.log(data.value);
 });
 </script>
 
@@ -86,7 +84,7 @@ onMounted(async () => {
     <!-- <div class="py-4 px-4 overflow-auto"> -->
     <UContainer class="py-4">
       <UCard class="shadow-lg">
-        <div class="w-60">
+        <div class="mb-2 w-60">
           <UInput
             id="table-quick-filter"
             @input="onTableFilterChange"
@@ -95,7 +93,7 @@ onMounted(async () => {
             icon="i-heroicons-magnifying-glass-20-solid"
           />
         </div>
-        
+
         <AgGridVue
           :class="{
             'ag-theme-alpine-dark': isDark,
@@ -126,7 +124,7 @@ onMounted(async () => {
         background-color: #171717,
         odd-row-background-color: transparent,
         data-color: #737373,
-        row-hover-color: rgb(115, 115, 115, 0.1)
+        row-hover-color: rgb(115, 115, 115, 0.1),
       ),
       alpine: (
         borders: none,
@@ -134,9 +132,9 @@ onMounted(async () => {
         background-color: #fff,
         odd-row-background-color: transparent,
         data-color: #737373,
-        row-hover-color: rgb(59, 130, 246, 0.2)
-      )
-    )
+        row-hover-color: rgb(59, 130, 246, 0.2),
+      ),
+    ),
   )
 );
 </style>
