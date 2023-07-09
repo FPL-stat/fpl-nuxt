068393c0b5a0dd1479976f5d3cb23891a81b8f19
Dave <bigwaive@gmail.com>
2023-07-09 22:09:03 +0200

    embed team info

--- server/api/test/index.get.ts
+++ server/api/test/index.get.ts
@@ -13,6 +13,38 @@ export default defineEventHandler(async (event) => {
           as: "team_info",
         },
       },
+      {
+        $unset: [
+          "team",
+          "team_code",
+          "team_info.draw",
+          "team_info.form",
+          "team_info.loss",
+          "team_info.played",
+          "team_info.points",
+          "team_info.position",
+          "team_info.pulse_id",
+          "team_info.strength",
+          "team_info.strength_attack_away",
+          "team_info.strength_attack_home",
+          "team_info.strength_defence_away",
+          "team_info.strength_defence_home",
+          "team_info.strength_overall_away",
+          "team_info.strength_overall_home",
+          "team_info.team_division",
+          "team_info.unavailable",
+          "team_info.updated_time",
+          "team_info.win",
+        ],
+      },
+      // {
+      //   $project: {
+      //     second_name: 1,
+      //     first_name: 1,
+      //     element_type: 1,
+      //     team_name: '$team_info.name',
+      //   },
+      // }
     ]);
 
     return result;
